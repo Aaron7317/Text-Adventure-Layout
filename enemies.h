5de73f3ea3a836ad255970321a77965419a6a0d0
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-02-01 22:12:55 -0800

    started work with enemies (may use namespaces to limit confusion with type enums

---
+++ enemies.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+//Little men with pitchforks and cat heads
+//Also big ants that wear shoes and have mustaches and glasses
+//a smoking pipe that has a funny face and arms but no legs so it Carrie's itself around by its arms
+
+enum EnemyType {
+    PITCHFORKCAT = 0, GIANTANT, PIPE
+};
+
+class Enemy {
+    private:
+        EnemyType type;
+        int level, health, damage;
+
+    public:
+        Enemy();
+};
