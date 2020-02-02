b4c35cd98ac7f38c8f4e75e8e2cfd3fd24a29555
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-02-01 21:55:40 -0800

    setting up rooms / need to do work on enemy branch first

--- rooms.cpp
+++ rooms.cpp
@@ -1,4 +1,39 @@
 #include "rooms.h"
 
 
-Room::
+Room::Room(std::string n, std::vector< std::pair<Direction, Room> > c, int f, Type t)
+    :name(n), connections(c), floor(f), roomType(t)
+    {
+    }
+
+Type Room::getType() {
+    return roomType;
+}
+
+void Room::initializeType() {
+    switch(roomType) {
+        case 0:
+            
+            break;
+        
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+        
+        case 4:
+            break;
+
+        default:
+            std::cout << "Error! Invalid Room Type";
+            break;
+    }
+}
+
+void Room::addConnection(std::pair<Direction, Room> newConnection) {
+    connections.push_back(newConnection);
+}
--- rooms.h
+++ rooms.h
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <vector>
 
+//TODO: Once enemy branch is done, add enemies to rooms
+
+//Node based connections
+
 enum Type {
-    INVALID = 0, EMPTY, TREASURE, SHRINE, SHOP, ARENA
+    STANDARD = 0, TREASURE, SHRINE, SHOP, ARENA
 };
 
 enum Direction {
     NORTH = 1, SOUTH, EAST, WEST
 };
 
-struct Room {
-    std::string name;
-    std::vector< std::pair<Direction, Room> > connections;
-    int floor;
-    Type roomType; 
-
-    Room(std::string n, std::vector< std::pair<Direction, Room> > c, int f, Type t); 
+class Room {
+    private:
+        
+        std::string name;
+        std::vector< std::pair<Direction, Room> > connections;
+        int floor;
+        Type roomType; 
+    
+    public:
+        Room(std::string n, std::vector< std::pair<Direction, Room> > c, int f, Type t); 
+        Type getType();
+        void initializeType();
+        void addConnection(std::pair<Direction, Room> newConnection);
 };
