2c7f970edb79541c47d213023fe2bb501a50f3cc
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-02-04 19:00:43 -0800

    made include folder... I will probably use a batch file to compile at some point

---
+++ include/rooms.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+
+//Node based connections
+
+
+enum Direction {
+    NORTH = 1, SOUTH, EAST, WEST, UP, DOWN
+};
+
+class Room { 
+        
+    public:
+
+        std::string name;
+        std::vector< std::pair<Direction, Room> > connections;
+
+        Room(std::string n); 
+
+        void addConnection(std::pair<Direction, Room> newConnection);
+        void addMultipleConnections(std::vector< std::pair<Direction, Room> > newConnectionVector);
+        std::vector< std::pair<Direction, Room> > getConnections();
+        //room method will be overriden for each room type
+        void roomMethod();
+};
+
+class Shrine : public Room {
+    public:
+        Shrine(std::string n);
+        void roomMethod();
+
+};
+
+class Shop : public Room {
+    public:
+        Shop(std::string n);
+        void roomMethod();
+};
+
