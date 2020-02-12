589a0ecd55b376e216b6bddaba685f0480e47f4d
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-02-11 20:54:55 -0800

    finished items for the most part I think

--- include/items/items.h
+++ include/items/items.h
@@ -8,11 +8,24 @@ class Item {
 
         Item(std::string n);
         Item();
+
+        void useItem();
 };
 
 
 class HealthConsumable : public Item {
+    public:
+        int healingAmount;
+
+        HealthConsumable(std::string n, int hA);
+        void useItem(Player& player);
 };
 
 class Weapon : public Item {
+    public:
+        int weaponDamage;
+
+        Weapon(std::string n, int wD);
+        void useItem();
+        void useItem(Enemy& target, Player player);
 };
--- src/items/items.cpp
+++ src/items/items.cpp
@@ -8,3 +8,30 @@ Item::Item(std::string n)
 {
 }
 
+void Item::useItem() {
+    std::cout << "You cannot do this now.\n";
+}
+
+HealthConsumable::HealthConsumable(std::string n, int hA) 
+:Item(n)
+{
+    healingAmount = hA;
+}
+
+void HealthConsumable::useItem(Player& player) {
+    player.addHealth(healingAmount);
+}
+
+Weapon::Weapon(std::string n, int wD) 
+:Item(n)
+{
+    weaponDamage = wD;
+}
+
+void Weapon::useItem() {
+    std::cout << "You cannot do this now.\n";
+}
+
+void Weapon::useItem(Enemy& target, Player player) {
+    target.attack(player.damage + weaponDamage);
+}
