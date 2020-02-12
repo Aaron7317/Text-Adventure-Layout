99952d4144692e2894493a7775997d3721b1f393
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-02-11 21:07:35 -0800

    think im ready to attempt to start merge into dev

--- include/game/game.h
+++ include/game/game.h
@@ -1,8 +1,21 @@
 #pragma once
-
+#include <iostream>
+#include <vector>
 
 enum GameState {
-    INTRO, STANDARDTURN, COMBATTURN,  
+    INTRO = 0, STANDARDTURN, COMBATTURN
 };
 
+enum BaseCommand {
+    MOVE = 0, INVENTORY, LOOKAROUND, SHOP
+};
 // make commandVector and maybe use switch with the index of command
+class Game {
+    private:
+        GameState currentState = INTRO;
+    public:
+        Game();
+
+        void setGameState(GameState newState);
+        GameState getGameState();
+};
